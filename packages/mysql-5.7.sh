fd5e982b6b1d8c2910e973f69294eb32fb849cc2
Mark Unsworth <markunsworth@gmail.com>
2017-12-22 12:35:06 +0000

    Update mysql-5.7.sh

--- packages/mysql-5.7.sh
+++ packages/mysql-5.7.sh
@@ -83,8 +83,8 @@ key_buffer		= 16M
 )
 
 
-
-for DB_NAME in ($MYSQL_DATABASES); do 
+DB_NAMES=($MYSQL_DATABASES);
+for DB_NAME in $DB_NAMES; do 
 	echo "Creating Database ${DB_NAME}"
 	"${MYSQL_DIR}/bin/mysql" --defaults-file="${MYSQL_DIR}/my.cnf" -u "${MYSQL_USER}" -e "CREATE DATABASE ${DB_NAME};"
 done
